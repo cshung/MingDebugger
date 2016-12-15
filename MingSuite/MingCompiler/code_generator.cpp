38fdac1cc101361c9c2d734d299bb32b671a7fc5
Andrew Au <cshung@gmail.com>
2016-12-14 20:55:28 -0800

    Beginning code generator implementation

--- MingSuite/MingCompiler/code_generator.cpp
+++ MingSuite/MingCompiler/code_generator.cpp
@@ -1,12 +1,24 @@
 #include "code_generator.h"
 
+#include <unordered_map>
+using namespace std;
+
+struct code_generation_context
+{
+    unordered_map<string, instruction*> functions;
+    label_instruction* epilog_label;
+};
+
 class code_generator_impl
 {
 public:
     code_generator_impl();
     ~code_generator_impl();
-    void generate_code(program_node* program);
+    instruction* generate_code(program_node* program);
 private:
+    instruction* generate_code(program_node* program, code_generation_context* context);
+    instruction* generate_code(function_node* function, code_generation_context* context);
+    instruction* generate_code(statement_node* statement, code_generation_context* context);
 };
 
 code_generator::code_generator()
@@ -19,9 +31,9 @@ code_generator::~code_generator()
     delete this->impl;
 }
 
-void code_generator::generate_code(program_node* program)
+instruction* code_generator::generate_code(program_node* program)
 {
-    this->impl->generate_code(program);
+    return this->impl->generate_code(program);
 }
 
 code_generator_impl::code_generator_impl()
@@ -32,7 +44,52 @@ code_generator_impl::~code_generator_impl()
 {
 }
 
-void code_generator_impl::generate_code(program_node* program)
+instruction* code_generator_impl::generate_code(program_node* program)
 {
-    // Do something!
+    code_generation_context context;
+    return this->generate_code(program, &context);
+}
+
+instruction* code_generator_impl::generate_code(program_node* program, code_generation_context* context)
+{
+    function_node* function = program->function;
+    while (function != nullptr)
+    {
+        instruction* function_body = this->generate_code(function, context);
+        context->functions.insert(make_pair(function->function_name, function_body));
+    }
+
+    // TODO: Layout and linking
+    return nullptr;
+}
+
+instruction* code_generator_impl::generate_code(function_node* function, code_generation_context* context)
+{
+    label_instruction* epilog_label = new label_instruction();
+    context->epilog_label = epilog_label;
+    instruction* statement_body = generate_code(function->statement, context);
+    return nullptr;
+}
+
+instruction* code_generator_impl::generate_code(statement_node* function, code_generation_context* context)
+{
+    // TODO: Implementation
+    return nullptr;
+}
+
+instruction::instruction()
+{
+    this->next = nullptr;
+}
+
+instruction::~instruction()
+{
+    if (this->next != nullptr)
+    {
+        delete this->next;
+    }
+}
+
+label_instruction::~label_instruction()
+{
 }
--- MingSuite/MingCompiler/code_generator.h
+++ MingSuite/MingCompiler/code_generator.h
@@ -4,12 +4,24 @@
 
 class code_generator_impl;
 
+struct instruction
+{
+    instruction();
+    virtual ~instruction();
+    instruction* next;
+};
+
+struct label_instruction : instruction
+{
+    virtual ~label_instruction();
+};
+
 class code_generator
 {
 public:
     code_generator();
     ~code_generator();
-    void generate_code(program_node* program);
+    instruction* generate_code(program_node* program);
 private:
     code_generator_impl* impl;
 };
